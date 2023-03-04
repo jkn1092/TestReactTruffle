3d7274dfe9e2c06f3cacaa88494575b71f725481
Jeremy N <jkn1092@gmail.com>
2023-03-04 16:38:29 +0100

    init

---
+++ client/src/components/Demo/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import useEth from "../../contexts/EthContext/useEth";
+import Title from "./Title";
+import Cta from "./Cta";
+import Contract from "./Contract";
+import ContractBtns from "./ContractBtns";
+import Desc from "./Desc";
+import NoticeNoArtifact from "./NoticeNoArtifact";
+import NoticeWrongNetwork from "./NoticeWrongNetwork";
+
+function Demo() {
+  const { state } = useEth();
+  const [value, setValue] = useState("?");
+  const [greet, setGreet] = useState("?");
+
+  const demo =
+    <>
+      <Cta />
+      <div className="contract-container">
+        <Contract value={value} greet={greet} />
+        <ContractBtns setValue={setValue} setGreet={setGreet} />
+      </div>
+      <Desc />
+    </>;
+
+  return (
+    <div className="demo">
+      <Title />
+      {
+        !state.artifact ? <NoticeNoArtifact /> :
+          !state.contract ? <NoticeWrongNetwork /> :
+            demo
+      }
+    </div>
+  );
+}
+
+export default Demo;
